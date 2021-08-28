99a994f14e6e47dacc51dd8d91c7ad79c3c00e75
Daniel Popescu <same.dan@gmail.com>
2021-08-28 16:02:11 +0200

    updated menu-items with react-router-dom

--- src/components/directory/directory-component.js
+++ src/components/directory/directory-component.js
@@ -1,59 +1,57 @@
-import React from 'react'
-import MenuItem from '../menu_item/menu-item.component';
-import './directory.styles.scss'
+import React from "react";
+import MenuItem from "../menu_item/menu-item.component";
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
-      sections:  [
+      sections: [
         {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: "shop/hats",
         },
         {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
-          linkUrl: 'shop/jackets'
+          linkUrl: "shop/jackets",
         },
         {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
-          linkUrl: 'shop/sneakers'
+          linkUrl: "shop/sneakers",
         },
         {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
           id: 4,
-          linkUrl: 'shop/womens'
+          linkUrl: "shop/womens",
         },
         {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
           id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
-    }
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
   }
 
-  render () {
+  render() {
     return (
-    <div className="directory-menu">
-      {
-        this.state.sections.map(({title, imageUrl, id, size}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-        ))
-      }
-    </div>
-    )
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
-export default Directory
+export default Directory;
