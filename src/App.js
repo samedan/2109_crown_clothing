5f0950f539ecd2bea252bf4339e148a127835ef5
Daniel Popescu <same.dan@gmail.com>
2021-08-31 11:21:29 +0200

    new pages- shop, collection-item, collection-preview and shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import {Route, Switch} from 'react-router-dom'
-
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => (
   <div>
     <h1>Hats Page</h1>
   </div>
-)
+);
 
 function App() {
   return (
-    <div >
+    <div>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
-     {/* <HomePage/> */}
+      {/* <HomePage/> */}
     </div>
   );
 }
