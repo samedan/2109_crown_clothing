fc37054d26ad1f26ca4439747525cbfc611a9d06
Daniel Popescu <same.dan@gmail.com>
2021-09-01 17:28:37 +0200

    redux part 1

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -64,7 +64,11 @@ class SignIn extends React.Component {
 
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+              type="button"
+            >
               Sign In With GOOGLE
             </CustomButton>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
+  <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
- ,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-
